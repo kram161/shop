b5159a581640ce7afefca7636d8ac4f4a5be0d7b
kram161 <39655243+kram161@users.noreply.github.com>
2021-05-05 16:56:51 +0300

    up

--- base/js/script.js
+++ base/js/script.js
@@ -5,12 +5,20 @@ $(function(){
     });
 });
 $(document).ready(function() {
-    $('.expand__sidebar').click(function(event){
+    $('.expand__sidebar').click(function(e){
         $('.sidebar').toggleClass('sidebar__active');
         $('.expand__sidebar').toggleClass('expand__sidebar__active');
     })
-    $('.expand__sidebar__active').click(function(event){
+    $('.expand__sidebar__active').click(function(e){
         $('.sidebar').removeClass('sidebar__active');
         $('.expand__sidebar').removeClass('expand__sidebar__active');
+    })
+    $('.sidebar__change-theme__button').click(function(e){
+        $('.sidebar__change-theme').toggleClass('sidebar__change-theme__white-theme')
+        $('.sidebar__logo').toggleClass('sidebar__logo__white-theme')
+        $('.sidebar__services').toggleClass('sidebar__services__white-theme')
+        $('.sidebar__menu').toggleClass('sidebar__menu__white-theme')
+        $('.sidebar').toggleClass('sidebar__white-theme')
+
     })
 });
