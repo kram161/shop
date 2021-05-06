1bd325281513f10fa64dc120e30a912e22342f60
kram161 <39655243+kram161@users.noreply.github.com>
2021-05-06 22:29:06 +0300

    upd

--- base/js/script.js
+++ base/js/script.js
@@ -10,12 +10,14 @@ $(document).ready(function() {
         $('.expand__sidebar').toggleClass('expand__sidebar__active');
         $('.main-content').toggleClass('Main-content__active')
         $('.main-navbar').toggleClass('main-navbar__active')
+        $('.product-customization__quantity__range').toggleClass('product-customization__quantity__range__active')
     })
     $('.expand__sidebar__active').click(function(e){
         $('.sidebar').removeClass('sidebar__active');
         $('.expand__sidebar').removeClass('expand__sidebar__active');
         $('.main-content').removeClass('Main-content__active')
         $('.main-navbar').removeClass('main-navbar__active')
+        $('.product-customization__quantity__range').removeClass('product-customization__quantity__range__active')
     })
     $('.sidebar__change-theme__button').click(function(e){
         $('.sidebar__change-theme').toggleClass('sidebar__change-theme__white-theme')
@@ -23,6 +25,11 @@ $(document).ready(function() {
         $('.sidebar__services').toggleClass('sidebar__services__white-theme')
         $('.sidebar__menu').toggleClass('sidebar__menu__white-theme')
         $('.sidebar').toggleClass('sidebar__white-theme')
+    })
+    $('.product-customization__platform__unit__activator').click(function(e){
+        $('.product-customization__platform__unit').toggleClass('product-customization__platform__unit__active')
+        $('.product-customization__platform__unit__description').toggleClass('product-customization__platform__unit__description__active')
+
     })
     $('.product-information__visible,.product-information__hidden').hover(function(e){
         $('.product-information__hidden').toggleClass('product-information__hidden__active')
@@ -35,7 +42,6 @@ let mySlider = new rSlider({
     scale: true,
     labels: true,
     set: [8],
-    width:932,
 });
 $(document).ready(function() {
     $('.down').click(function () {
