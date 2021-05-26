077ae2642433b285dccdcf8188bdb30747565995
kram161 <39655243+kram161@users.noreply.github.com>
2021-05-26 22:31:20 +0300

    upd

--- base/js/script.js
+++ base/js/script.js
@@ -58,6 +58,7 @@ $(document).ready(function() {
         $('.shopping-cart__basket-units__one').toggleClass('shopping-cart__basket-units__one__white-theme')
         $('.shopping-cart__basket__forms').toggleClass('shopping-cart__basket__forms__white-theme')
         $('.shopping-cart__basket__payment').toggleClass('shopping-cart__basket__payment__white-theme')
+        $('.profile').toggleClass('profile__white-theme')
     })
     $('.product-customization__platform__unit__activator').click(function(e){
         $('.product-customization__platform__unit').toggleClass('product-customization__platform__unit__active')
