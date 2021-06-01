3bea1da7544837f875a2b27a24b8ec5ad7fa425c
kram161 <39655243+kram161@users.noreply.github.com>
2021-06-02 02:35:22 +0300

    upd

--- base/js/swiperSlider.js
+++ base/js/swiperSlider.js
@@ -1,26 +1,40 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+    slidesPerView: 4,
+    allowSlideNext:true,
+    allowSlidePrev:true,
+    allowTouchMove:true,
+    loop:true,
     breakpoints: {
         "@0.00": {
             slidesPerView: 1,
-            spaceBetween: 10,
         },
         "@0.75": {
             slidesPerView: 2,
-            spaceBetween: 20,
         },
         "@1.00": {
             slidesPerView: 3,
-            spaceBetween: 40,
         },
         "@1.50": {
             slidesPerView: 4,
-            spaceBetween: 50,
         },
+        1440: {
+            slidesPerView: 4,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        540: {
+            slidesPerView: 2,
+        },
+        425: {
+            slidesPerView: 1,
+        }
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
 });
